309d32a57fdc05db83af45c03763d9370c6dc8d2
Wenchao HUANG <767575096@qq.com>
2023-10-11 16:00:01 +0000

    Update catalog
    - add serializer on catalog.models.Book
    - add simple views on catalog

--- backend/catalog/urls.py
+++ backend/catalog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('books/', views.books),
+    path('books/<int:pk>', views.book_detail),
 ]
--- backend/catalog/views.py
+++ backend/catalog/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from django.http import JsonResponse
+from rest_framework.response import Response
+
+from .serializers import BookSerializer
+from .models import Book
 
 # Create your views here.
+@api_view(["GET"])
+def books(request):
+    books = Book.objects.all()
+    serializer = BookSerializer(books, many=True)
+    return Response(
+        data=serializer.data,
+        status=status.HTTP_200_OK
+    )
+
+@api_view(["GET", "DELETE"])
+def book_detail(request, pk):
+    try:
+        book = Book.objects.get(pk=pk)
+    except:
+        return JsonResponse(
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    if (request.method == "GET"):
+        serializer = BookSerializer(book)
+        return Response(
+            data=serializer.data,
+
+        )
+    elif (request.method == "DELETE"):
+        book.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
